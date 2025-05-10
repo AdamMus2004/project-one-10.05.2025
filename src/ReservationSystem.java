9e2b7652158b5ce392e426489e171eb485a33ecf
AdamMus2004 <adammuspraca@gmail.com>
2025-05-10 23:51:25 +0200

    feat: add cinema.txt

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import javax.sound.midi.SoundbankResource;
+import java.util.Scanner;
 
 public class ReservationSystem {
+    public static void main(String[] args) {
+        Seat seat = new Seat();
+        Scanner scanner = new Scanner(System.in);
+        boolean running =true;
+
+        while (running) {
+            System.out.println("Choose option:\n" +
+                    "1. Check if seat is avalible.\n" +
+                    "2. Reserve seat.\n" +
+                    "3. Cancel reservation.\n" +
+                    "4. Show seats\n" +
+                    "5. Quit.");
+            if (!scanner.hasNextInt()){
+                scanner.next();
+                continue;
+            }
+            int option = scanner.nextInt();
+            if (option<1||option>5) throw new IllegalArgumentException("Bad choice in menu!");
+            if (option==1){
+                System.out.println("Enter row and collumn to check if seat is avalible:");
+                if (seat.isSeatAvailable(scanner.nextInt(), scanner.nextInt())){
+                    System.out.println("Seat is available.");
+                }
+                else {
+                    System.out.println("Seat is taken.");
+                }
+            }
+            if (option==2){
+                System.out.println("Enter row and collumn to reserve seat:");
+                boolean success = seat.reserveSeat(scanner.nextInt(), scanner.nextInt());
+                System.out.println(success ? "Seat reserved!" : "Seat already taken!"   );
+
+            }
+            if (option==3){
+                System.out.println("Enter row and collumn to cancel reservation of the seat:");
+                boolean canceled=seat.cancelReservation(scanner.nextInt(), scanner.nextInt());
+                System.out.println(canceled?"Reservation canceled." : "Seat was not reserved).");
+            }
+            if (option==4){
+                seat.printSeats();
+            }
+            if (option==5){
+                System.out.println("Thanks, see you soon!");
+                running=false;
+            }
+        }
+    scanner.close();
+    }
 }
--- src/Seat.java
+++ src/Seat.java
@@ -1,2 +1,87 @@
+import java.io.*;
+
 public class Seat {
+    private int[][] seats= new int[10][10];
+    public Seat() {
+        loadFromFile();
+    }
+    void loadFromFile(){
+        File file = new File("cinema");
+        if (!file.exists()) return;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("cinema"))){
+            for (int i = 0; i < 10; i++) {
+                String line = reader.readLine();
+                if (line!=null&&line.length()>=10) {
+                    for (int j = 0; j < 10; j++) {
+                        seats[i][j]=Character.getNumericValue(line.charAt(j));
+                    }
+                }
+
+            }
+        }catch (IOException e){
+            System.out.println("Error with loading file"+e.getMessage());
+        }
+
+    }
+    void saveToFile(){
+        try (PrintWriter writer=new PrintWriter (new FileWriter("cinema"))){
+        for (int[]row:seats){
+            for (int seat:row){
+                writer.print(seat);
+            }
+            writer.println();
+        }
+        }catch (IOException e){
+            System.out.print("Error in saving cinema file"+e.getMessage());
+        }
+    }
+    boolean isSeatAvailable(int row, int col)
+    {
+        if (row>10||col>10||row<1||col<1) {
+            throw new IllegalArgumentException("Rows and columns must be between 1 and 10.");
+        }
+
+        return seats[row-1][col-1]==0;
+    }
+    boolean reserveSeat(int row, int col){
+        if (row>10||col>10||row<1||col<1) {
+            throw new IllegalArgumentException("Rows and columns must be between 1 and 10.");
+        }
+        if (seats[row-1][col-1]==0) {
+            seats[row - 1][col - 1] = 1;
+            saveToFile();
+            return true;
+        }
+        else return false;
+
+    }
+    boolean cancelReservation(int row, int col){
+        if (row>10||col>10||row<1||col<1) {
+            throw new IllegalArgumentException("Rows and columns must be between 1 and 10.");
+        }
+        if (seats[row-1][col-1]==1) {
+            seats[row - 1][col - 1] = 0;
+            saveToFile();
+            return true;
+        }
+        else return false;
+    }
+    void printSeats(){
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Rząd: "+(i+1));
+            for (int j = 0; j < 10; j++) {
+
+                if (seats[i][j]==1){
+
+                System.out.print("|X| ");
+                }
+                else {
+                    System.out.print("|O| ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
